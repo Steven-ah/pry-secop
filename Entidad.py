714eaf29c4991cacbf8c87c4ae5426b92f9dbd23
Steven Alvarez Holguin <steven-ah@hotmail.com>
2024-01-27 13:42:19 -0500

    Se carga la tabla, solo retirando duplicados, a la base de datos

--- Entidad.py
+++ Entidad.py
@@ -1,11 +1,13 @@
 import credentials as cd
+import numpy as np
 import pandas as pd
 import pyodbc
 import uuid
 
 def connectBD():
      try:
-          cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+cd.server+';DATABASE='+cd.database+';UID='+cd.username+';PWD='+ cd.password)
+          cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+cd.server+';DATABASE='+cd.database+';UID='+cd.username+';PWD='+ cd.password+';Authentication=SqlPassword;TrustServerCertificate=Yes')
+          print("Se ha conectado de forma exitosa con la base de datos")
           return cnxn
      except Exception as e:
           print(f'Error al conectar con la base de datos. {e}')
@@ -16,7 +18,7 @@ def insertData(df):
           cxn = connectBD()
           cursor = cxn.cursor()
           for i, row in df.iterrows():
-               cursor.execute("INSERT INTO Entidad VALUES(?,?,?,?,?,?)",
+               cursor.execute("INSERT INTO [Entidad] ([IdEntidad], [NIT], [Nombre], [Departamento], [Ciudad], [Orden], [Centralizada]) VALUES(?,?,?,?,?,?,?)",
                                row.idEntidad
                                ,row['Nit Entidad']
                                ,row.Entidad
@@ -32,15 +34,15 @@ def insertData(df):
           cursor.close()
           cxn.close()
 
-df = pd.read_csv('SECOP_II_-_Procesos_de_Contrataci_n.csv') # ! Debido a que tenemos que sacar los datos desde la fuente, nos vemos obligados a tomar todo el archivo, aunque solo necesitemos ciertas columnas
-
-df =  df[['Entidad', 'Nit Entidad', 'Departamento Entidad', 'Ciudad Entidad', 'OrdenEntidad', 'Entidad Centralizada']] # * Tomamos solo las columnas de la entidad
+df = pd.read_csv('SECOP_II_-_Procesos_de_Contrataci_n.csv', usecols=['Entidad', 'Nit Entidad', 'Departamento Entidad', 'Ciudad Entidad', 'OrdenEntidad', 'Entidad Centralizada'])
 
 df = df.drop_duplicates() # * Se toman los registros únicos.
 
-df['idEntidad'] = uuid.uuid4() # * Se crea la columna del uniqueidentifier para su inserción sobre la base de datos.
+df['idEntidad'] = [uuid.uuid4() for _ in range(len(df))] # * Se crea la columna del uniqueidentifier para su inserción sobre la base de datos.
 
 df['Entidad Centralizada'] = df['Entidad Centralizada'].replace('Si', True)
 df['Entidad Centralizada'] = df['Entidad Centralizada'].replace('No', False)
+
+df = df.replace({np.nan: None}) # ? Se agrega debido a que la columa Entidad tiene un valor vacío
 
 insertData(df)
